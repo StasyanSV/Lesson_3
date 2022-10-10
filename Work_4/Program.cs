4d225cf5394ba0caa028fe14cb2a5ba1f5bd9e25
StasyanSV <stanislav_xx@mail.ru>
2022-10-10 16:28:18 +0400

    add Work_4

---
+++ Work_4/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая
+// принимает на вход число (N) и выдаёт таблицу
+// квадратов чисел от 1 до N.
+
+Console.Clear();
+
+Console.Write("Введите целое число N: ");
+int N = int.Parse(Console.ReadLine());
+
+int i = 1;
+while (i <= N)
+{
+    Console.Write(Math.Pow(i,2) + " ");
+    i++;
+}
