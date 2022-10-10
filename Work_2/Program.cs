c8f697d80ab71ddaa3f0103745ace611427a43a1
StasyanSV <stanislav_xx@mail.ru>
2022-10-10 15:49:27 +0400

    add Work_2

--- Work_2/Program.cs
+++ Work_2/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая по
+// заданному номеру четверти, показывает диапазон
+// возможных координат точек в этой четверти (x и y).
+
+Console.Clear();
+
+Console.Write("Введите номер четверти ( 1, 2, 3 или 4): ");
+int number_1 = int.Parse(Console.ReadLine());
+
+switch (number_1)
+{
+    case 1:
+        Console.WriteLine("Первой четверти соответсвует следующий диапазон координат: ");
+        Console.Write("X >= 0 и Y >=0 ");
+        break;
+    case 2:
+        Console.WriteLine("Второй четверти соответсвует следующий диапазон координат: ");
+        Console.Write("X <= 0 и Y >=0 ");
+        break;
+    case 3:
+        Console.WriteLine("Третьей четверти соответсвует следующий диапазон координат: ");
+        Console.Write("X <= 0 и Y <=0 ");
+        break;
+    case 4:
+        Console.WriteLine("Четвертой четверти соответсвует следующий диапазон координат: ");
+        Console.Write("X >= 0 и Y <=0 ");
+        break;
+    default:
+        Console.Write("Введенное число не соответсвует условию задачи!");
+        break;
+}
